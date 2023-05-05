668ab8d9a0b3b98e7bfb07049595cd838f927a41
samsiana27 <128759922+samsiana27@users.noreply.github.com>
2023-05-05 22:50:43 +0700

    Add files via upload

---
+++ Latihan1.php
@@ -0,0 +1,17 @@
+<?php
+class Latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "Selamat Datang.. selamat belajar Web Programming";
+        //$this->view('view_latihan1');
+    }
+
+    public function penjumlahan($n1, $n2)
+    {
+        $this->load->model('Model_latihan1');
+        $hasil = $this->Model_latihan1->jumlah($n1, $n2);
+        echo "Hasil Penjumlahan dari". $n1 ." + ". $n2 ." = "
+.$hasil;     
+    }
+}
